c9ee20d3dbec91d423150a6bef9ef5e771d4982e
vlvkdn4107 <102271558+vlvkdn4107@users.noreply.github.com>
2022-05-19 17:58:14 +0900

    음..

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -24,9 +24,13 @@ public class MainActivity extends AppCompatActivity {
     private TextView zero;
     private TextView ce;
     private TextView plus;
-    private TextView result;
+    private TextView result1;
+    private TextView c_Result;
 
+    private TextView equal;
+    private TextView multi;
 
+    String calculation = "";
     String newValue = "0";
     String oldValue = "0";
 
@@ -51,7 +55,10 @@ public class MainActivity extends AppCompatActivity {
         zero = findViewById(R.id.zero);
         ce = findViewById(R.id.ce);
         plus = findViewById(R.id.plus);
-        result = findViewById(R.id.result);
+        result1 = findViewById(R.id.result1);
+        c_Result = findViewById(R.id.c_Result);
+        equal = findViewById(R.id.equal);
+        multi = findViewById(R.id.multi);
 
 
 
@@ -62,8 +69,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d("MYTAG", "one 이 눌러 졌어요");
+//                oldValue = oldValue + "1";
+//                c_Result.setText(oldValue);
                 newValue = newValue + "1";
-                result.setText(newValue);
+                result1.setText(newValue);
 //                if(result.getText().equals("0")){
 //                    result.setText("");
 //                }
@@ -76,7 +85,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.d("MYTAG", "two 이 눌러 졌어요");
                 newValue = newValue + "2";
-                result.setText(newValue);
+                result1.setText(newValue);
+//                c_Result.setText(result1.getText() + newValue);
 //                if(result.getText().equals("0")){
 //                    result.setText("");
 //                }
@@ -91,13 +101,13 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //            result.setText(result.getText() + "3");
             newValue = newValue + "3";
-            result.setText(newValue);
+            result1.setText(newValue);
 
         });
         four.setOnClickListener(view ->{
             Log.d(TAG, "four 가 눌러 졌어요");
             newValue = newValue + "4";
-            result.setText(newValue);
+            result1.setText(newValue);
 //            if(result.getText().equals("0")){
 //                result.setText("");
 //            }
@@ -106,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
         five.setOnClickListener(view ->{
             Log.d(TAG, "five 가 눌러 졌어요");
             newValue = newValue + "5";
-            result.setText(newValue);
+            result1.setText(newValue);
 //            if(result.getText().equals("0")){
 //                result.setText("");
 //            }
@@ -115,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
         six.setOnClickListener(view ->{
             Log.d(TAG, "six 가 눌러 졌어요");
             newValue = newValue + "6";
-            result.setText(newValue);
+            result1.setText(newValue);
 //            if(result.getText().equals("0")){
 //                result.setText("");
 //            }
@@ -124,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
         seven.setOnClickListener(view ->{
             Log.d(TAG, "seven 가 눌러 졌어요");
             newValue = newValue + "7";
-            result.setText(newValue);
+            result1.setText(newValue);
 //            if(result.getText().equals("0")){
 //                result.setText("");
 //            }
@@ -133,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
         eight.setOnClickListener(view ->{
             Log.d(TAG, "eight 가 눌러 졌어요");
             newValue = newValue + "8";
-            result.setText(newValue);
+            result1.setText(newValue);
 //            if(result.getText().equals("0")){
 //                result.setText("");
 //            }
@@ -142,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
         nine.setOnClickListener(view ->{
             Log.d(TAG, "nine 가 눌러 졌어요");
             newValue = newValue + "9";
-            result.setText(newValue);
+            result1.setText(newValue);
 //            if(result.getText().equals("0")){
 //                result.setText("");
 //            }
@@ -151,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
         zero.setOnClickListener(view ->{
             Log.d(TAG, "zero 가 눌러 졌어요");
             newValue = newValue + "0";
-            result.setText(newValue);
+            result1.setText(newValue);
 //            if(result.getText().equals("0")){
 //                result.setText("");
 //            }
@@ -161,27 +171,41 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "ce 가 눌러 졌어요");
             newValue = "0";
             oldValue = "0";
-            result.setText("0");
+            result1.setText("");
+//            c_Result.setText("");
         });
         plus.setOnClickListener(view ->{
             // 이미 구분은 가능함
             Log.d(TAG, "plus 가 눌러 졌어요");
+            newValue = newValue + "+";
+            result1.setText(newValue);
+            // 1. dje
+            // 2.
+        });
+
+        multi.setOnClickListener(v -> {
+            newValue = newValue + "×";
+            result1.setText(newValue);
+        });
+
+        equal.setOnClickListener(v -> {
             // newValue -> String 이라서 형변환 해야된다
             // 1. 1눌렀을때 newValue에 1이라는 값이 담겨 져있다
             // 2.
             int number1 = Integer.parseInt(newValue);
             int number2 = Integer.parseInt(oldValue);
             int sum = (number1 + number2);
-            Log.d(TAG, "number1 :" + number1);
-            Log.d(TAG, "number2 :" + number2);
-            Log.d(TAG, "sum :" + sum);
-
             oldValue = String.valueOf(sum);
-            newValue = "0";
-            result.setText(oldValue);
+            c_Result.setText(oldValue);
+//            if(result.getText().equals("+")){
+//
+//            }
+
+            Log.d(TAG, " 먹히나?" + number1);
+            Log.d(TAG, " 먹히나?" + number2);
+
+
         });
-
-
         
     }
 
