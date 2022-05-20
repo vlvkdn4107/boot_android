4c7cfdfaec94c915fa63bcc5d031547d9c0e1cc2
vlvkdn4107 <102271558+vlvkdn4107@users.noreply.github.com>
2022-05-20 18:11:01 +0900

    버튼꾸미기

---
+++ mySpace/app/src/main/java/com/example/myspace/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constrain3);
+    }
+}
